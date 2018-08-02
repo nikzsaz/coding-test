fe2ccfdba8a3e55fc9e122e22f20c6d71116f352
Niraj Sazzie <mr.nirajjee@gmail.com>
2018-08-02 21:31:45 +0530

    Update Shipment.java

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -1,9 +1,30 @@
 package com.monsanto.mbt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shipment {
+    public static final int MAX_SIZE = 10;
+    private List<Widget> widgets = new ArrayList<>();
 
+    public void addWidget(Widget widget) {
+        if (isFull()) {
+            throw new RuntimeException("Shipment cannot contain more than " + MAX_SIZE + " widgets");
+        } else {
+            widgets.add(widget);
+        }
+    }
 
+    public boolean isFull() {
+        if (widgets.size() == MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-
+    public List<Widget> getWidgets() {
+        return this.widgets;
+    }
 
 }
